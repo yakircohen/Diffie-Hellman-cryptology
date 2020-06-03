dbf4ac42937e1564e2a35008bec0b45b1fabf1ab
yakir cohen <yakir cohen@10.0.0.6>
2020-06-03 14:34:55 +0300

    bjhbj

--- Diffie Helman Cryptology/src/KeyExchange.java
+++ Diffie Helman Cryptology/src/KeyExchange.java
@@ -1,4 +1,17 @@
+import java.math.BigInteger;
+
 
 public class KeyExchange {
-
+       private long a;
+       private long b;
+       private long p;
+       
+       
+       
+       
+       
+       
+       
+       
+       
 }
