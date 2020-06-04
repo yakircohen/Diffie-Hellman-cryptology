5b9f003193417b6e4b1fe644880e9476926cb49a
ramidman <ramidman@gmail.com>
2020-06-04 14:03:34 +0300

    Elliptic Curve

---
+++ Diffie Helman Cryptology/src/Person.java
@@ -0,0 +1,28 @@
+import java.math.BigInteger;
+
+public class Person {
+	private BigInteger privateKey;
+	private BigInteger pubKeyX;
+	private BigInteger pubKeyY;
+	
+	public Person(){
+		
+	}
+	
+	
+	
+	
+	public BigInteger getPubKeyX() {
+		return pubKeyX;
+	}
+	public void setPubKeyX(BigInteger pubKeyX) {
+		this.pubKeyX = pubKeyX;
+	}
+	public BigInteger getPubKeyY() {
+		return pubKeyY;
+	}
+	public void setPubKeyY(BigInteger pubKeyY) {
+		this.pubKeyY = pubKeyY;
+	}
+
+}
